88aa46b074631218cc0d50b1e1805f5e6d412b18
tal0311 <tal.amit0311@gmail.com>
2022-08-25 21:14:05 +0300

    better mail-list

--- src/app/cmps/mail-list/mail-list.component.ts
+++ src/app/cmps/mail-list/mail-list.component.ts
@@ -1,32 +1,16 @@
+import { Observable } from 'rxjs';
 import { MailService } from './../../services/mail.service';
 import { Component, OnInit } from '@angular/core';
+import { Mail } from 'src/app/models/mail';
 
 @Component({
   selector: 'app-mail-list',
   templateUrl: './mail-list.component.html',
   styleUrls: ['./mail-list.component.scss'],
 })
-export class MailListComponent implements OnInit {
-  // props
-
-  // emits
-
-  // services
-  constructor(private mailService: MailService) {}
-  // data
-  mails: any;
-  // created
-  ngOnInit(): void {
-    this.mailService.query();
-    console.log('mail-list');
-
-    this.mailService.mails$.subscribe((mails) => {
-      this.mails = mails;
-    });
+export class MailListComponent {
+  mails$!: Observable<Mail[]>;
+  constructor(private mailService: MailService) {
+    this.mails$ = mailService.mails$;
   }
-  // data
-
-  // methods
-
-  // computed
 }
